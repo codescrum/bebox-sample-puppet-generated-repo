a7479160c0b30b8734ad53bffcdb91049d5d6664
Juan Pablo Amaya <jp.amaya@codescrum.com>
2014-08-12 17:54:43 -0500

    Create profile simple/ruby with 'bebox profile new ruby -p simple' command

---
+++ puppet/profiles/simple/ruby/manifests/init.pp
@@ -0,0 +1,17 @@
+# The profile class include the classes instantiation or
+# puppet/modules type resource definitions.
+# It can include hiera calls for the parametter setting.
+# Example:
+# class profiles::category::technology {
+#   $technology_parameters = hiera("technology")
+#   $parameter1 = technology_parameters[parameter1]
+#   ...
+#   class { "technology_resource":
+#     parameter1  => $parameter1
+#     ...
+#   }
+# }
+
+class profiles::simple::ruby {
+
+}
