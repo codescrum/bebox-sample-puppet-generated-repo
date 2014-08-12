91843bc12b9b0a4b5dfba3c65704e484f089c8c5
Juan Pablo Amaya <jp.amaya@codescrum.com>
2014-08-12 17:15:52 -0500

    Project creation with 'bebox new sample-puppet-generated-repo' command

---
+++ config/deploy/vagrant.rb
@@ -0,0 +1,37 @@
+set(:deploy_via, :copy_subdir)
+set(:deploy_subdir, 'puppet')
+# Select options accord to the phase parameter
+if phase == 'node_prepare'
+	set :user, "vagrant"
+	set :ssh_key, '~/.vagrant.d/insecure_private_key'
+	set :copy_exclude, ['/profiles', '/roles', '/steps']
+  set(:deploy_to) { "/home/vagrant/code" }
+elsif phase == 'step-0'
+  set :user, "vagrant"
+  set :ssh_key, '~/.vagrant.d/insecure_private_key'
+  set :copy_exclude, ['/lib', '/prepare', '/profiles', '/roles']
+  set(:deploy_to) { "/home/vagrant/code" }
+else
+  set :user, "puppet"
+  set :ssh_key, 'config/keys/environments/vagrant/id_rsa'
+  set :copy_exclude, ['/lib', '/prepare', '/profiles', '/roles']
+  set(:deploy_to) { "/home/puppet/code" }
+end
+# set :deploy_via, :copy
+set :scm, :none
+set :repository, "."
+
+
+set :ssh_options, {
+  :keys => ssh_key,
+  :port => '22',
+  :forward_agent => 'true'
+}
+
+# Start nodes definitions
+# End nodes definitions
+
+desc 'Test the vagrant connection by doing issuing a simple echo command'
+task :probe, roles: [:web, :app, :db] do
+  run 'echo $PATH'
+end
